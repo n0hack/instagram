22d4ceded8d3674e5d68d26cfef21aef3849b9db
Ming <nohack-@naver.com>
2023-04-10 17:03:38 +0900

    [chore] API 개념 검증

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+  type Movie {
+    title: String
+    year: Int
+  }
+
+
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({ title: 'Hello', year: 2023 }),
+  },
+  Mutation: {
+    createMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+    deleteMovie: (_, args) => {
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀 Server ready at ${url}`);
