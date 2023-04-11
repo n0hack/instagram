ec99cae332a0495e385a74e6d78a8f9d3a11e85a
Ming <nohack-@naver.com>
2023-04-11 17:39:52 +0900

    [feat] 프로필 업데이트

--- backend/users/editProfile/editProfile.resolvers.js
+++ backend/users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,29 @@
+import bcrypt from 'bcrypt';
+import client from '../../client';
+
 export default {
   Mutation: {
     editProfile: async (_, { firstName, lastName, username, email, password }) => {
-      console.log('hi');
+      let hashedPassword;
+      if (password) {
+        hashedPassword = await bcrypt.hash(password, 10);
+      }
+
+      const updatedUser = await client.user.update({
+        where: { id: 2 },
+        data: { firstName, lastName, username, email, ...(hashedPassword && { password: hashedPassword }) },
+      });
+
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: '프로필 업데이트가 불가능합니다.',
+        };
+      }
     },
   },
 };
